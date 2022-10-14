e1d6f52c7b6e42aedfcc8e402bebc0f70f13302d
Alex <alexrollins95@gmail.com>
2022-10-13 20:53:00 -0500

    ensure overlap between each wall's yPos

--- Assets/Scenes/Scripts/Spawner.cs
+++ Assets/Scenes/Scripts/Spawner.cs
@@ -49,7 +49,7 @@ public class Spawner : MonoBehaviour
         // Creating this next wall should set a new prev wall, so the next check won't pass and will wait until we're closer as well
         if ((prevWall.transform.position.x - notBird.transform.position.x) < spawnDistance)
         {
-            // TODO: distance between should really be based off of the height of each one
+            // TODO: distance between should really be based off of the height of each one (or more accurately, the length of the gap between each one)
             createWall(prevWall.transform.position.x + Random.Range(minDistanceBetween, maxDistanceBetween));
         }
 
@@ -71,9 +71,7 @@ public class Spawner : MonoBehaviour
     GameObject getWallClone(float xPos)
     {
         GameObject clonedWall = Instantiate(objectToSpawn, transform.position, transform.rotation);
-        // TODO: Height randomization should depend on the previous wall, so that they overlap at least a little bit every time, but not too much
-        float randomHeight = Random.Range(minHeight, maxHeight);
-        clonedWall.transform.localScale = new Vector3(objectToSpawn.transform.localScale.x, randomHeight, 1);
+        clonedWall.transform.localScale = new Vector3(objectToSpawn.transform.localScale.x, getRandomHeight(), 1);
         if (nextIsFloor)
         {
             clonedWall.transform.position = new Vector3(xPos, getYForFloorWall(clonedWall), -1);
@@ -84,6 +82,32 @@ public class Spawner : MonoBehaviour
         }
         nextIsFloor = !nextIsFloor;
         return clonedWall;
+    }
+
+    float getRandomHeight()
+    {
+        float nextMinHeight = minHeight;
+        float nextMaxHeight = maxHeight;
+
+        // If there's a previous wall, then change the min height of the next one so that it will overlap at least a little bit.
+        // We shouldn't be able to draw a horizontal line along the x axis from the previous wall without hitting the next wall.
+        if (prevWall == null)
+        {
+        }
+        else if (nextIsFloor)
+        {
+            // prev was ceiling
+            float prevLowerEnd = prevWall.transform.position.y - (prevWall.transform.localScale.y / 2);
+            nextMinHeight = System.Math.Abs(prevLowerEnd - minSpawnLocation);
+        }
+        else
+        {
+            // prev was floor
+            float prevHigherEnd = prevWall.transform.position.y + (prevWall.transform.localScale.y / 2);
+            nextMinHeight = System.Math.Abs(prevHigherEnd - maxSpawnLocation);
+        }
+
+        return Random.Range(nextMinHeight, nextMaxHeight);
     }
 
     // Get the Y location for the wall if it's coming from the floor
